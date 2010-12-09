58e921b646c458c4d4aafcb3396036652a12e196
zhangzhenshan <zhenshan@ubuntu.(none)>
2010-12-09 13:26:49 +0800

    1

---
+++ 11.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void say_hello_to_you(void)
+{
+  printf("Hello World\n");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+int main(int argc, const char *argv[])
+{
+    say_hello_to_you();
+    return 0;
+}
